df2b352dc9a8563e72a60b01612c5ed4139d256b
Tyler Gregorcyk <Tyler@Gregorcyk.com>
2024-02-01 13:56:16 -0600

    changed get functions to use single paramater

--- generate_api.sh
+++ generate_api.sh
@@ -2,7 +2,10 @@
 export JAVA_OPTS="-DmaxYamlCodePoints=9999999"
 
 java $JAVA_OPTS -jar openapi-generator-cli-7.0.1.jar generate \
-	--config "${PWD}/config.yaml" \
+	-g rust \
+	-p useSingleRequestParameter=true \
+	--package-name netbox \
+	--package-version 0.0.1 \
 	--input-spec "${PWD}/netbox_openapi.yaml" \
 	--output "${PWD}/api_output"
 
